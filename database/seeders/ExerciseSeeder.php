12c67fecb7fdd5e1601ebe91a50cfd190ee50f6d
Jewel Aniciete <jewelaniciete@gmail.com>
2024-12-02 22:03:05 +0800

    FIX: Gym Service

--- database/seeders/ExerciseSeeder.php
+++ database/seeders/ExerciseSeeder.php
@@ -14,51 +14,94 @@ class ExerciseSeeder extends Seeder
     public function run(): void
     {
         Exercise::create([
-            "name" => "Gym Workout per Session",
-            "price" => 80.00,
+            "name" => "Gym per Session",
+            "price" => 60.00,
             "tag" => "session",
+            'short_description' => 'Gym Workout per Session'
         ]);
 
         Exercise::create([
-            "name" => "Gym Workout per Month",
+            "name" => "Gym per Month",
+            "price" => 750.00,
+            "tag" => "monthly",
+            'short_description' => 'Gym Workout per Month'
+        ]);
+
+        Exercise::create([
+            "name" => "Monthly Tredmill",
+            "price" => 550.00,
+            "tag" => "monthly",
+            'short_description' => 'Treadmill per Month'
+        ]);
+
+        Exercise::create([
+            "name" => "Gym + Treadmill",
             "price" => 1200.00,
             "tag" => "monthly",
-        ]);
-
-        Exercise::create([
-            "name" => "Treadmill per Session",
-            "price" => 100.00,
-            "tag" => "session",
-        ]);
-
-        Exercise::create([
-            "name" => "Treadmill per Month",
-            "price" => 1200.00,
-            "tag" => "session",
+            'short_description' => 'Gym + Treadmill per Month'
         ]);
 
         Exercise::create([
             "name" => "P.I per Session",
-            "price" => 100.00,
+            "price" => 120.00,
             "tag" => "session",
+            'short_description' => 'Gym Daily + Personal Instructor per Session'
         ]);
 
         Exercise::create([
             "name" => "P.I per Month",
-            "price" => 1200.00,
-            "tag" => "session",
+            "price" => 1500.00,
+            "tag" => "monthly",
+            'short_description' => 'Gym Daily + Personal Instructor per Month'
         ]);
 
         Exercise::create([
-            "name" => "Boxing per Session",
-            "price" => 100.00,
+            "name" => "Zumba",
+            "price" => 70.00,
             "tag" => "session",
+            'short_description' => 'Zumba per Session'
         ]);
 
         Exercise::create([
-            "name" => "Boxing per Month",
-            "price" => 1200.00,
+            "name" => "Dance Studio for Regular",
+            "price" => 30.00,
             "tag" => "session",
+            'short_description' => 'Dance Studio for Regular'
+        ]);
+
+        Exercise::create([
+            "name" => "Dance Studio for Student",
+            "price" => 15.00,
+            "tag" => "session",
+            'short_description' => 'Dance Studio for Regular'
+        ]);
+
+        Exercise::create([
+            "name" => "Muay Thai",
+            "price" => 250.00,
+            "tag" => "session",
+            'short_description' => 'Muay Thai per Session'
+        ]);
+
+        Exercise::create([
+            "name" => "Taekwando per Session",
+            "price" => 400.00,
+            "tag" => "session",
+            'short_description' => 'Taekwando per Session'
+        ]);
+
+        Exercise::create([
+            "name" => "Taekwando per Month",
+            "price" => 3000.00,
+            "tag" => "session",
+            'short_description' => 'Taekwando per Month'
+        ]);
+
+        Exercise::create([
+            "name" => "Boxing",
+            "price" => 250.00,
+            "tag" => "session",
+            'short_description' => 'Boxing per Session'
         ]);
     }
 }
